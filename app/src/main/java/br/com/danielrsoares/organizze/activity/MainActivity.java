1d32724dc2e208b511706b4b766bce19447559b8
Daniel R Soares <danielrsoares2006@gmail.com>
2018-04-30 02:00:49 -0300

    Versão:1.0.0

--- app/src/main/java/br/com/danielrsoares/organizze/activity/MainActivity.java
+++ app/src/main/java/br/com/danielrsoares/organizze/activity/MainActivity.java
@@ -1,18 +1,10 @@
 package br.com.danielrsoares.organizze.activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
-
 import br.com.danielrsoares.organizze.R;
-import br.com.danielrsoares.organizze.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
-
-    int b;
-    int hh;
 
 
     @Override
