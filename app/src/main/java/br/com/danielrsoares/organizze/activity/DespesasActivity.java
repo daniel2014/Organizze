3d844bb261f6b395a7f5fcbe4c847ea72393b2ba
Daniel R Soares <danielrsoares2006@gmail.com>
2018-05-08 21:19:09 -0300

    Data: 07-05-2018 V_1.0.8 Adicionado Método 'validarCamposDespesas' para Válidar se os campos das Despesa estão todos preenchidos antes de salvar no FireBase. ====================================================================== Atualizações Android Studio: Google Play services revision 49 Instant Apps Development SDK revision 1.2.0

--- app/src/main/java/br/com/danielrsoares/organizze/activity/DespesasActivity.java
+++ app/src/main/java/br/com/danielrsoares/organizze/activity/DespesasActivity.java
@@ -33,17 +33,64 @@ public class DespesasActivity extends AppCompatActivity {
         campoData.setText(DateCustom.dataAtual());
     }
 
-    public void salvarDespesa(View view){
-        movimentacao = new Movimentacao();
-        String data = campoData.getText().toString();
-        movimentacao.setValor(Double.parseDouble(campoValor.getText().toString())); //Pegando o Valor digitado pelo usuário
-        movimentacao.setCategoria(campoCategoria.getText().toString());//Pegando Categoria informada pelo usuário
-        movimentacao.setDescricao(campoDescricao.getText().toString()); //Pegando pegando a descrição informada pelo usuário
-        movimentacao.setData(data); //Pegando a Data informada pelo usuário
-        movimentacao.setTipo("d"); //Configura o Tipo de Movimentação (d) = Despesa
+    //Movimentações serão salvas no FireBase
+    public void salvarDespesa(View view) {
+        if (validarCamposDespesas()) {
 
-        movimentacao.salvar(data); //Esse Método pegará todas as informações definida na Classe Movimentacao para salvar no FireBase
+            movimentacao = new Movimentacao();
+            String data = campoData.getText().toString();
+            movimentacao.setValor(Double.parseDouble(campoValor.getText().toString())); //Pegando o Valor digitado pelo usuário
+            movimentacao.setCategoria(campoCategoria.getText().toString());//Pegando Categoria informada pelo usuário
+            movimentacao.setDescricao(campoDescricao.getText().toString()); //Pegando pegando a descrição informada pelo usuário
+            movimentacao.setData(data); //Pegando a Data informada pelo usuário
+            movimentacao.setTipo("d"); //Configura o Tipo de Movimentação (d) = Despesa
 
-        Toast.makeText(getApplicationContext(),"Salvando...", Toast.LENGTH_LONG).show();
+            movimentacao.salvar(data); //Esse Método pegará todas as informações definida na Classe Movimentacao para salvar no FireBase
+
+            Toast.makeText(getApplicationContext(), "Salvando...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Método para Validar os Campos Preenchidos na Despesa antes de Salvar - Incluído dentro do método salvar
+    public Boolean validarCamposDespesas() {
+
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescrição = campoDescricao.getText().toString();
+
+        if (!textoValor.isEmpty()) {
+            if (!textoData.isEmpty()) {
+                if (!textoCategoria.isEmpty()) {
+                    if (!textoDescrição.isEmpty()){
+                        return true;// Padrão é retornar verdadeiro (true) ele servirá como um disparador caso os campos estejam todos preenchidos
+                        //dentro do método salvarDespesa irá salvar caso for verdadeiro
+
+                    }else {
+                        Toast.makeText(DespesasActivity.this,
+                                "Descrição não foi preenchido",
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                } else {
+                    Toast.makeText(DespesasActivity.this,
+                            "Categoria não foi preenchida",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } else {
+                Toast.makeText(DespesasActivity.this,
+                        "Data não foi preenchido",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(DespesasActivity.this,
+                    "Valor não foi preenchido,",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        //return true; foi colocado esse retorno aqui apenas para não dar erro na hora de contruir esse código de validação
     }
 }
